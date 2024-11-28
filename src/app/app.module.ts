676020f6c569257fe015bcfd524e9c8467a7bc5d
Naderab <abdellaouinader@gmail.com>
2024-11-28 13:41:38 +0100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,28 @@ import { AppComponent } from './app.component';
 import { ResidencesComponent } from './components/residences/residences.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ResidenceDetailsComponent } from './components/residences/residence-details/residence-details.component';
+import { AddResidenceComponent } from './components/residences/add-residence/add-residence.component';
+import { ApartmentsComponent } from './components/apartments/apartments/apartments.component';
+import { ApartmentsByResidenceComponent } from './components/apartments/apartments-by-residence/apartments-by-residence.component';
+import { AddApartmentComponent } from './components/apartments/add-apartment/add-apartment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ResidencesComponent,
-    HeaderComponent
+    HeaderComponent,
+    NotFoundComponent,
+    HomeComponent,
+    FooterComponent,
+    ResidenceDetailsComponent,
+    AddResidenceComponent,
+    ApartmentsComponent,
+    ApartmentsByResidenceComponent,
+    AddApartmentComponent
   ],
   imports: [
     BrowserModule,
