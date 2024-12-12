da352948a97ac22f331de76be17068aefa523a93
Naderab <abdellaouinader@gmail.com>
2024-12-12 09:50:31 +0100

    Service

--- src/app/components/residences/add-residence/add-residence.component.ts
+++ src/app/components/residences/add-residence/add-residence.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ResidenceService } from 'src/app/services/residence.service';
 
 @Component({
   selector: 'app-add-residence',
@@ -8,26 +9,28 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AddResidenceComponent implements OnInit {
 
+  constructor(private r:ResidenceService){}
   //<form></form>
   residence!: FormGroup;
 
   ngOnInit(): void {
-    this.residence =
-      new FormGroup({
+    this.residence = new FormGroup({
       //<input>
-      name: new FormControl("",[Validators.required,Validators.minLength(3)]),
-        address: new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      /*address: new FormGroup({
           street: new FormControl(),
           city:new FormControl()
-      }),
+      }),*/
+      address: new FormControl(),
       image: new FormControl(),
-      status:new FormControl()
-      })
+      status: new FormControl(),
+    });
   }
 
   add() {
     console.log(this.residence.get('name')!.value)
     console.log(this.residence.value)
+    this.r.addResidence(this.residence.value);
   }
 
 }
--- src/app/components/residences/residences.component.ts
+++ src/app/components/residences/residences.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Residence } from '../../core/models/residence';
+import { ResidenceService } from 'src/app/services/residence.service';
 
 @Component({
   selector: 'app-residences',
@@ -12,37 +13,9 @@ export class ResidencesComponent implements OnInit {
   hide: boolean = false;
   selectedResidence: Residence = new Residence();
   searchText: string = '';
+  constructor(private r:ResidenceService){}
   ngOnInit() {
-    this.residences = [
-      {
-        id: 1,
-        name: 'El fel',
-        address: 'Borj Cedria',
-        image: '../../assets/images/R1.jpg',
-        status: 'Disponible',
-      },
-      {
-        id: 2,
-        name: 'El yasmine',
-        address: 'Ezzahra',
-        image: '../../assets/images/R2.jpg',
-        status: 'Disponible',
-      },
-      {
-        id: 3,
-        name: 'El Arij',
-        address: 'Rades',
-        image: '../../assets/images/R3.jpg',
-        status: 'Vendu',
-      },
-      {
-        id: 4,
-        name: 'El Anber',
-        address: 'inconnu',
-        image: '../../assets/images/R4.jpg',
-        status: 'En Construction',
-      },
-    ];
+    this.residences = this.r.residences;
     console.log(this.hide);
     console.log(this.selectedResidence);
   }
